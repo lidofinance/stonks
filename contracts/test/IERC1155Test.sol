d75e11f87846e8c596a33e90f202f3f9f14573a9
Evgenii <dev-www@ya.ru>
2023-12-02 00:42:23 +0800

    feat: added tests for ERC721 and ERC1155 recover

---
+++ contracts/test/IERC1155Test.sol
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract NFT_1155 is ERC1155 {
+    constructor(string memory uri) ERC1155(uri) {
+        _mint(msg.sender, 0, 10, "");
+        _mint(msg.sender, 1, 10**27, "");
+        _mint(msg.sender, 2, 10**18, "");
+    }
+}
