349dbc24915f9f5667a5463192b8c2e5abf957f1
Bogdan Kovtun <psirex48@gmail.com>
2023-12-27 16:36:22 +0400

    Use the variables empty in the scripts by default

--- scripts/deploy-cow-swap-vault-relayer-stub.ts
+++ scripts/deploy-cow-swap-vault-relayer-stub.ts
@@ -6,7 +6,7 @@ import { confirmOrAbort } from '../utils/prompt'
 import { getDeployer, verify } from '../utils/deployment'
 import { CoWSwapVaultRelayerStub__factory } from '../typechain-types/factories/contracts/stubs/CoWSwapVaultRelayerStub.sol'
 
-const OWNER: string = '0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c'
+const OWNER: string = ''
 const MANAGER: string = ethers.ZeroAddress
 
 assert(ethers.isAddress(OWNER), 'OWNER address is invalid')
